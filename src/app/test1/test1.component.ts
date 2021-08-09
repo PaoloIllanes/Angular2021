280390ca9738b8d9e6e300f97620c5def9f9f0cb
PaoloIllanes <oeuthfoierughi@gmail.com>
2021-08-09 18:53:51 -0500

    3 files modified, 3 added

--- src/app/test1/test1.component.ts
+++ src/app/test1/test1.component.ts
@@ -1,15 +1,17 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, Input, Output} from '@angular/core';
 
 @Component({
   selector: 'comp1-test1',
   templateUrl: './test1.component.html',
   styleUrls: ['./test1.component.css']
 })
-export class Test1Component implements OnInit {
+export class Test1Component {
 
+
+  @Input ('status') statusTest: string='working';
   constructor() { }
 
-  ngOnInit() {
-  }
+  @Output() onSendData  new= EventEmitter(null);
 
+  onClickTest(event:any){}
 }
