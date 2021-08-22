28fe5fb5fd577528f5576ea1f8834e4bfff72855
PaoloIllanes <oeuthfoierughi@gmail.com>
2021-08-22 10:22:44 -0500

    08/12/21 observer of

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
-
+import {of} from 'rxjs';
+import { filter, map } from 'rxjs/operators';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -9,10 +10,34 @@ export class AppComponent {
   title = 'Angular2021';
   myStatus = 'my status';
   sw=true;
-
+  tictock = of([1,2,3,4,5]);
   constructor(){
+
+    // PERSON A
+    this.tictock.pipe(
+      map(s => s.join('-')),
+      map(s => s + 'hola'),
+    ).subscribe(v => {
+      console.log('PERSON A VIDEO', v);
+    });
+    // PERSON B
+    this.tictock.pipe(
+      filter((v:any) => v[0]%2 === 1)
+    ).subscribe(v => {
+      console.log('PERSON B VIDEO', v);
+    });
+    // PERSON C
+    this.tictock.subscribe(v => {
+      console.log('PERSON C VIDEO', v);
+    });
+    
   }
-  printDataRicardoComp(event:any){
-    console.log('Event:', event);
+  onAddVideo(){
+
   }
+  printData(event:any){
+    console.log('RICARDO COMP:', event);
+  }  
+
+  
 }
