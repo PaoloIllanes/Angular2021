58a38cd2bf310025739ae84ecdffed1f61e7a3dd
PaoloIllanes <oeuthfoierughi@gmail.com>
2021-08-22 21:08:38 -0400

    2 files modified, 27 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,7 +8,7 @@ import { Test2Component } from './test2/test2.component';
 import { Test3Component } from './test3/test3.component';
 import { Directive1Directive } from './directive1.directive';
 import { Directive2Directive } from './directive2.directive';
-import { Module1Module } from './module1/module1.module';
+
 @NgModule({
   declarations: [
     AppComponent,
@@ -21,7 +21,7 @@ import { Module1Module } from './module1/module1.module';
   imports: [
     BrowserModule,
     FormsModule,
-    Module1Module
+  
   ],
   providers: [],
   bootstrap: [AppComponent]
