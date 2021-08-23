606809509da6587b3edf5958f88aa884ea2d219e
PaoloIllanes <oeuthfoierughi@gmail.com>
2021-08-23 17:41:58 -0400

    1. Listar las personas en 2 categorías vacunadas y no vacunadas

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,73 +1,35 @@
 
 import { Component, ElementRef, ViewChild } from '@angular/core';
-import {BehaviorSubject, of, Subscription} from 'rxjs';
-import { filter, map, delay } from 'rxjs/operators';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'Angular2021';
-  myStatus = 'my status';
-  sw=true;
 
-  personASub:Subscription;
-  video = 1;
-  tictock = new BehaviorSubject(this.video);
 
-  @ViewChild('myDiv1') myDiv1: ElementRef;
-  @ViewChild('myDiv2') myDiv2: ElementRef;
-  @ViewChild('comp1') comp1: any;
+  personasTotal = [
+    {name: "juan", age: 16, date: "2019-09-07T15:50+00Z", disease: true, vaccineType: "A", vaccined:0, doses: 0},
+    {name: "maria", age: 23, date: "2019-09-07T15:50+00Z", disease: false, vaccineType: "B", vaccined:1, doses: 2},
+    {name: "carla", age: 45, date: "2019-09-07T15:50+00Z", disease: true, vaccineType: "C", vaccined:0, doses: 0},
+    {name: "marco", age: 50, date: "2019-09-07T15:50+00Z", disease: false, vaccineType: "A", vaccined:1, doses: 1},
+    {name: "marta", age: 12, date: "2019-09-07T15:50+00Z", disease: false, vaccineType: "A", vaccined:0, doses: 0},
+    {name: "jorge", age: 36, date: "2019-09-07T15:50+00Z", disease: false, vaccineType: "C", vaccined:1, doses: 3},
+    {name: "maritza", age: 18, date: "2019-09-07T15:50+00Z", disease: false, vaccineType: "C", vaccined:0, doses: 0},
+    {name: "leonardo", age: 35, date: "2019-09-07T15:50+00Z", disease: true, vaccineType: "B", vaccined:0, doses: 0},
+    {name: "ramiro", age: 24, date: "2019-09-07T15:50+00Z", disease: false, vaccineType: "B", vaccined:1, doses: 2},
+    {name: "juana", age: 13, date: "2019-09-07T15:50+00Z", disease: false, vaccineType: "A", vaccined:0, doses: 0}
+   ]
+
+   personas = {
+    vacunados: this.personasTotal.filter(p => p.vaccined<0),
+    novacunados: this.personasTotal.filter(p => p.vaccined=0)
+  };
+ 
 
   constructor(){
 
-    // PERSON A
-    this.personASub = this.tictock.pipe(
-      filter(s => s%2 === 0)
-    ).subscribe(v => {
-      console.log('PERSON A VIDEO', v);
-    });
-    // PERSON B
-    this.tictock.pipe(
-      delay(4000)
-    ).subscribe(v => {
-      console.log('PERSON B VIDEO', v);
-    });
-    // PERSON C
-    this.tictock.subscribe(v => {
-      console.log('PERSON C VIDEO', v);
-    });
-    
-  }
-  onAddVideo(){
-    this.video ++
-    this.tictock.next(this.video);    
-
-  }
-  person1Unsubscribe(){
-    this.personASub.unsubscribe();
-    console.log('PERSON A SE DESUSCRIBE')
- }
-  printData(event:any){
-    console.log('COMP:', event);
-  }  
-
-  test(event){
-    console.log('AAAAAAAAAAAAAAAAAAAAAAAAAAAA', event);
-  }
-  
-  test100(event){
-    console.log('234234234234234', event);
-
-
-  }
-  onShowLocalVars(){
-    console.log(this.myDiv1, this.myDiv2, this.comp1);
-  
-    this.comp1.onClickTest();
-  
-    this.myDiv2.nativeElement.value = 'paolo';
   }
   
   }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,30 +3,21 @@ import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { Test1Component } from './test1/test1.component';
-import { Test2Component } from './test2/test2.component';
-import { Test3Component } from './test3/test3.component';
+
 import { Directive1Directive } from './directive1.directive';
 import { Directive2Directive } from './directive2.directive';
-import { HomeModule } from './pages/home/home.module';
-import { BuyModule } from './pages/buy/buy.module';
-import { AdminModule } from './pages/admin/admin.module';
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    Test1Component,
-    Test2Component,
-    Test3Component,
     Directive1Directive,
     Directive2Directive
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HomeModule,
-    BuyModule,
-    AdminModule
+  
   
   ],
   providers: [],
