fd864e6c16d9204f72f76c0c4887d714a9f4fe2f
PaoloIllanes <oeuthfoierughi@gmail.com>
2021-09-09 17:29:26 -0400

    9/9/21 facebook template

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -6,7 +6,7 @@ import { PublicationService } from './services/publication.service';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  styleUrls: ['./login.component.scss']
 })
 export class LoginComponent implements OnInit {
 
