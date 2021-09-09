054bfdf0d5022befd2b0adf367b9887ac022af82
Paolo <oeuthfoierughi@gmail.com>
2021-09-09 12:22:30 -0400

    test

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,23 @@
-import { Component } from '@angular/core';
 
+import { Component, ElementRef, ViewChild } from '@angular/core';
+import { Router } from '@angular/router';
+import {BehaviorSubject, of, Subscription} from 'rxjs';
+import { filter, map, delay } from 'rxjs/operators';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'Angular2021';
-}
 
+
+
+constructor(private router: Router){}
+
+
+  
+  }
+
+  
+
+  
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,31 @@
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 
+import { RouterModule, Routes } from '@angular/router';
+
+
+const routes : Routes =[
+  {path:'',redirectTo:'login',pathMatch:'full'},
+  {path:'login',loadChildren: () => import('./login/login.module').then(m=>m.LoginModule)},
+  {path:'pages',loadChildren: () => import('./pages/pages.module').then(m=>m.PagesModule)},
+
+]
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+   
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    RouterModule.forRoot(routes),
+    
+  
+  
   ],
   providers: [],
   bootstrap: [AppComponent]
