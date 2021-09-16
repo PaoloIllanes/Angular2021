4e4063fb5a45cbc94e63ce1c49361ec6cecdb2e8
PaoloIllanes <oeuthfoierughi@gmail.com>
2021-09-16 17:09:23 -0400

    8 files added, 2 deleted

--- src/app/services/transaction.service.ts
+++ src/app/services/transaction.service.ts
@@ -1,12 +1,11 @@
 import {Injectable} from '@angular/core';
-import {environment} from "../../environments/environment";
 import {HttpClient} from "@angular/common/http";
 import {Observable} from "rxjs";
 
 @Injectable()
 export class TransactionService {
 
-  private url = environment.app.apiBaseUrl;
+  private url = environment.app.apiBaseUrl
 
   constructor(private http: HttpClient) {
   }
