c009c4886e47c58b0be9452b0d4c97b3316f031e
PaoloIllanes <oeuthfoierughi@gmail.com>
2021-09-16 16:55:54 -0400

    4 files modified, 3 added, 13 deleted

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,17 +3,72 @@ import { Component, ElementRef, ViewChild } from '@angular/core';
 import { Router } from '@angular/router';
 import {BehaviorSubject, of, Subscription} from 'rxjs';
 import { filter, map, delay } from 'rxjs/operators';
+import { TransactionService } from '../services/transaction.service';
+import { WalletService } from '../services/wallet.service';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+  wallets: any[] = [];
+  transactions: any[] = []
 
+  constructor(private walletService: WalletService,
+              private transactionService: TransactionService) {
+  }
 
+  ngOnInit(): void {
+    this.loadWallets();
+    this.loadTransactions();
+  }
 
-constructor(private router: Router){}
+  onMine(transaction: any): void {
+    const walletFROM = this.wallets.find(w => w.wallet === transaction.from);
+    const walletTO = this.wallets.find(w => w.wallet === transaction.to);
 
+    walletFROM[transaction.moneyType] = walletFROM[transaction.moneyType] - transaction.quantity;
+    walletTO[transaction.moneyType] = walletTO[transaction.moneyType] + transaction.quantity;
+
+    this.transactionService.delete(transaction.id).subscribe(() => this.loadTransactions());
+    this.walletService.update(walletFROM.id, walletFROM).subscribe(res => this.loadWallets());
+    this.walletService.update(walletTO.id, walletTO).subscribe(res => this.loadWallets());
+
+    /*forkJoin(
+      this.walletService.update(walletFROM.id, walletFROM),
+      this.walletService.update(walletTO.id, walletTO)
+    ).pipe(
+      exhaustMap(res => {
+        console.log('RES EXHA', res);
+        return this.transactionService.delete(transaction.id)
+      })
+    ).subscribe(res => {
+      console.log('FORK JOIN', res)
+      //this.transactionService.delete(transaction.id).subscribe(() => this.loadTransactions())
+    })*/
+
+  }
+
+  getTransactionsStatus(): boolean {
+    const aux = this.transactions.filter(t => t.mineType === 'PoS' && t.miner < 20);
+    return this.transactions.length === aux.length;
+  }
+
+  getTotalCoin(type: string): number {
+    return this.wallets.reduce((acc, value) => acc + (value[type] > 0 ? value[type] : 0), 0);
+  }
+
+  loadTransactions(): void {
+    this.transactionService.getAll().subscribe(res =>
+      this.transactions = Object.entries(res).map((s: any) => ({id: s[0], ...s[1]}))
+    );
+  }
+
+  loadWallets(): void {
+    this.walletService.getAll().subscribe(res =>
+      this.wallets = Object.entries(res).map((s: any) => ({id: s[0], ...s[1]}))
+    );
+  }
 
   
   }
