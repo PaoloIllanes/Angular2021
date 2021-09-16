94feb9c0770196e2cfbdf439169a281f200a2b47
PaoloIllanes <oeuthfoierughi@gmail.com>
2021-09-16 15:49:10 -0400

    - 10/09/2021 creation of auth interceptor

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   app: {
-    apiBaseUrl: "https://facebook-angular-course-default-rtdb.firebaseio.com"
+    apiBaseUrl: ""
   },
   auth: {
     apiBaseUrl: "https://identitytoolkit.googleapis.com",
